239435f0bf26fe9d29bee83628d81eec49c30e80
364985 <c36498511@gmail.com>
2022-03-16 00:15:12 +0800

    Updates

---
+++ precache-manifest.d8a4377881f867d7207ef526981d542f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1516a5d41cae9669639f",
+    "url": "/testing-application/static/css/main.9a8ae319.chunk.css"
+  },
+  {
+    "revision": "1516a5d41cae9669639f",
+    "url": "/testing-application/static/js/main.1516a5d4.chunk.js"
+  },
+  {
+    "revision": "4f473d823a9b02f8d234",
+    "url": "/testing-application/static/js/1.4f473d82.chunk.js"
+  },
+  {
+    "revision": "d1b5fe89998db0cca2b8",
+    "url": "/testing-application/static/js/2.d1b5fe89.chunk.js"
+  },
+  {
+    "revision": "6ab84825ff8d6073fd05",
+    "url": "/testing-application/static/js/runtime~main.6ab84825.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/testing-application/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "438d8e82a009463a658d51b25c9fa7d9",
+    "url": "/testing-application/index.html"
+  }
+];
