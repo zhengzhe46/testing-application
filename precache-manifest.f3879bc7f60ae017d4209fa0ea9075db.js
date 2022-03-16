4dd43e58472e2734224c36ab4ad0c81ded80413c
364985 <c36498511@gmail.com>
2022-03-16 15:23:26 +0800

    Updates

--- precache-manifest.f3879bc7f60ae017d4209fa0ea9075db.js
+++ precache-manifest.f3879bc7f60ae017d4209fa0ea9075db.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "a15233132ed8ff9d06bd",
+    "revision": "90c148c860f7f53b73f6",
     "url": "/testing-application/static/css/main.0ac7d511.chunk.css"
   },
   {
-    "revision": "a15233132ed8ff9d06bd",
-    "url": "/testing-application/static/js/main.a1523313.chunk.js"
+    "revision": "90c148c860f7f53b73f6",
+    "url": "/testing-application/static/js/main.90c148c8.chunk.js"
   },
   {
     "revision": "75aff281b66c352666e7",
     "url": "/testing-application/static/js/1.75aff281.chunk.js"
   },
   {
-    "revision": "30838d6307b644ae7131",
-    "url": "/testing-application/static/js/2.30838d63.chunk.js"
+    "revision": "5d996eac51815b38abe6",
+    "url": "/testing-application/static/js/2.5d996eac.chunk.js"
   },
   {
     "revision": "2a9edf2088d411541f9b",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/testing-application/static/media/logo.06e73328.svg"
   },
   {
-    "revision": "051ba5a90dc838c1dffbaeefe6af96db",
+    "revision": "899c5b8689ceff71ab37cfd9a311f92e",
     "url": "/testing-application/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/testing-application/precache-manifest.2f06a544421dd7d00989a726ba673d58.js"
+  "/testing-application/precache-manifest.f3879bc7f60ae017d4209fa0ea9075db.js"
 );
 
 workbox.clientsClaim();
